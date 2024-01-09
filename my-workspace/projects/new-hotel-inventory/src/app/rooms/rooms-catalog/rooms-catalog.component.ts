526765c231865be023d91170a3a1270e90b4bfd5
Linux-Maniac <151807519+Linux-Maniac@users.noreply.github.com>
2024-01-09 07:48:02 +0530

    Update rooms-catalog.component.ts with missing properties

--- my-workspace/projects/new-hotel-inventory/src/app/rooms/rooms-catalog/rooms-catalog.component.ts
+++ my-workspace/projects/new-hotel-inventory/src/app/rooms/rooms-catalog/rooms-catalog.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RoomDetails } from '../rooms';
 
 @Component({
   selector: 'app-rooms-catalog',
   templateUrl: './rooms-catalog.component.html',
   styleUrl: './rooms-catalog.component.scss'
 })
-export class RoomsCatalogComponent {
-
+export class RoomsCatalogComponent implements OnInit{
+  @Input() roomsCatalog: RoomDetails[]=[];
+  constructor(){}
+  ngOnInit(): void {}
 }
